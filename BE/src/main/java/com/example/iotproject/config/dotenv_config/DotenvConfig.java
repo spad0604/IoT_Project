18218d95dbefb98b80ae3840cd0dac1b4e5096e1
spad0604 <giap.nv225126@sis.hust.edu.vn>
2025-01-22 22:34:53 +0700

    update

---
+++ BE/src/main/java/com/example/iotproject/config/dotenv_config/DotenvConfig.java
@@ -0,0 +1,13 @@
+//package com.example.iotproject.config.dotenv_config;
+//
+//import io.github.cdimascio.dotenv.Dotenv;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+//@Configuration
+//public class DotenvConfig {
+//    @Bean
+//    public Dotenv dotenv() {
+//        return Dotenv.configure().directory(System.getProperty("user.dir")).load();
+//    }
+//}
--- BE/src/main/java/com/example/iotproject/config/mqtt_config/MqttConfig.java
+++ BE/src/main/java/com/example/iotproject/config/mqtt_config/MqttConfig.java
@@ -1,18 +1,17 @@
 package com.example.iotproject.config.mqtt_config;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.eclipse.paho.client.mqttv3.MqttClient;
 import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
 import org.eclipse.paho.client.mqttv3.MqttException;
 import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Lazy;
 
 @Configuration
 public class MqttConfig {
     public MqttClient mqttClient() throws MqttException {
-        String broker = "tcp://localhost:1883";
+        String broker = "tcp://13.211.227.100:1883";
         String clientId = "test-client";
         MqttClient mqttClient = new MqttClient(broker, clientId, new MemoryPersistence());
         MqttConnectOptions options = new MqttConnectOptions();
